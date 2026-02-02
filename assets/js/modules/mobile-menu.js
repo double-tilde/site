9e5262acf111ce67940ff1fe3ae7682ba66937a9
Double Tilde <double-tilde@protonmail.com>
2026-02-02 18:24:58 +0000

    fix: make mobile links close menu on same page destination

--- assets/js/modules/mobile-menu.js
+++ assets/js/modules/mobile-menu.js
@@ -1,15 +1,49 @@
+function closeMenuAfterScroll(link, menu) {
+  link.addEventListener('click', (e) => {
+    if (link.hash) {
+      let urlClean = link.href;
+      let urlHash = link.hash;
+      let res = urlClean.slice(0, -urlHash.length);
+
+      console.log(res);
+      console.log(window.location.href);
+
+      if (res == window.location.href) {
+        e.preventDefault();
+        if (urlHash) {
+          urlHash.scrollIntoView({ behavior: 'smooth' });
+          setTimeout(() => {
+            menu.classList.remove('mobile-animate');
+          }, 100);
+        }
+        return;
+      }
+    }
+
+    if (link.href !== window.location.href) {
+      window.location.href = link.href;
+      return;
+    }
+  });
+}
+
 export function toggleMobileMenu() {
   const navMobile = document.getElementById('nav-mobile');
   const mobileOpenBtn = document.getElementById('mobile-open-btn');
   const mobileCloseBtn = document.querySelectorAll('.mobile-close-btn');
+  const mobileLinks = document.querySelectorAll('.nav-mobile > ul > li > a');
 
   mobileCloseBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
-      navMobile.classList.toggle('mobile-animate');
+      navMobile.classList.remove('mobile-animate');
     });
   });
 
+  mobileLinks.forEach((link) => {
+    closeMenuAfterScroll(link, navMobile);
+  });
+
   mobileOpenBtn.addEventListener('click', () => {
-    navMobile.classList.toggle('mobile-animate');
+    navMobile.classList.add('mobile-animate');
   });
 }
